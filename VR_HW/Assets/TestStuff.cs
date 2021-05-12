38ba2df9ee1d90f20d67a3fb7bc4b9d2403517d5
pj-99 <62024696+pj-99@users.noreply.github.com>
2021-05-12 18:01:26 +0800

    先用ENTER代替按鈕了 :(

---
+++ VR_HW/Assets/TestStuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+public class TestStuff : MonoBehaviour
+{
+    public Button enterNameButton = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Keyboard.current[Key.Enter].wasPressedThisFrame) {
+            return;
+        }
+        enterNameButton.onClick.Invoke();
+        Debug.Log("enter is presseed");
+
+       
+        
+    }
+}
